00093fa63e79e7375c42458488e11a56fa686a10
Your Name <youremail@domain.com>
2024-09-24 12:49:03 +0300

    functiom

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - 42); // Calculate the absolute distance
+}
+
+function distanceFromHqInFeet(somevalue) {
+    return distanceFromHqInBlocks(somevalue)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const distanceInBlocks = Math.abs(destination - start);
+    return distanceInBlocks * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const distanceInFeet = Math.abs(destination - start) * 264;
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
